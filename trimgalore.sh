e13cba9373fac396810f904c6a3ffe44eb6589f6
ibryant2 <ibryant@unmc.edu>
2025-04-28 15:02:56 -0500

    Update trimgalore.sh

--- trimgalore.sh
+++ trimgalore.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
-# Define an array with the sample identifiers (e.g., ut, e6, e6_aux, aux)
+# Define an array with the sample identifiers [e.g. ("esc_k27me3" "esc_igg")].
 samples=("esc_k27me3")
 
-# Loop through each sample
+# Loop through each sample.
 for sample in "${samples[@]}"; do
-    # Define input files based on the sample
+    # Define input files based on the sample.
     input1="${sample}_1.fastq"
     input2="${sample}_2.fastq"
 
-    # Define output SAM file and log file based on the sample
+    # Define output basename and log file for your trimmed output samples.
     base="${sample}"
     log_output="tg.${sample}.out"
 
-    # Run the bowtie2 command for each sample
+    # Run the bowtie2 command for each sample.
 
     trim_galore --paired "$input1" "$input2" --basename "$base" --cores 6 2>> "$log_output"
 
